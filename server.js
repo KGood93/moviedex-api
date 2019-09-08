f0eb63e0e03505b1a42907e41d8cf73b82de2dc7
KA Goodreau <goodreau.kath@gmail.com>
2019-09-08 16:33:23 -0500

    added auth token

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const MOVIES = require('./movie-data-small.json')
 
+console.log(process.env.API_TOKEN)
+
 const app = express()
 
 app.use(morgan('dev'))
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+
+    next()
+})
 
 app.get('/movie', function handleGetMovie(req, res) {
     let response = MOVIES
